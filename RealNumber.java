ff75f17208bcf1944707181e0bb714adaa751fca
will-o-the-wisp <kli18@stuy.edu>
2018-10-16 11:01:51 -0400

    fixed

--- RealNumber.java
+++ RealNumber.java
@@ -39,17 +39,3 @@ public class RealNumber{
     return ""+value;
   }
 }
-public class RealNumber{
-  private double val;
-  public RealNumber(double x){
-  }
-  public float getVal(){
-    return 0;
-  }
-  public String toString(){
-    return "";
-  }
-  public int compareTo(RealNumber x){
-    return 0;
-  }
-}
